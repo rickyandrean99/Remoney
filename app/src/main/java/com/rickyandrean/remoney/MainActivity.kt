5ad7a8ead56fb6bfe45980159b0a9bbc3d80125d
rickyandrean99 <ricky.ralph09@gmail.com>
2022-03-29 03:37:13 +0700

    Header section done

--- app/src/main/java/com/rickyandrean/remoney/MainActivity.kt
+++ app/src/main/java/com/rickyandrean/remoney/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.rickyandrean.remoney
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -9,7 +10,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.rickyandrean.remoney.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,17 +18,30 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Hide support action bar
         supportActionBar?.hide()
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Connect fragment for each page to navigation controller
+        val navController = findNavController(R.id.navigationHost)
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_expenses,
                 R.id.navigation_loans,
             )
         )
-
         setupActionBarWithNavController(navController, appBarConfiguration)
-        binding.navView.setupWithNavController(navController)
+        binding.bottomNavigation.setupWithNavController(navController)
+
+        // Event listener
+        binding.cvNotification.setOnClickListener(this)
+        binding.cvSetting.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.cvNotification -> Toast.makeText(this, "Ini Notification", Toast.LENGTH_SHORT).show()
+            R.id.cvSetting -> Toast.makeText(this, "Ini Setting", Toast.LENGTH_SHORT).show()
+            R.id.cvFriend -> Toast.makeText(this, "Ini Friend", Toast.LENGTH_SHORT).show()
+        }
     }
 }
